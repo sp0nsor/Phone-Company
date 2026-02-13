f4504d7cbfd5303079c19e71d1b6e9b6dae12521
Raman Kakotkin <t3rixis@gmail.com>
2026-02-13 16:18:56 +0300

    chore: add-eslint

--- frontend/phone-company/src/app/app.ts
+++ frontend/phone-company/src/app/app.ts
@@ -1,10 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
   protected readonly title = signal('phone-company');
