7398950bd8b31b5dd0ee54ecb7d535ae377edf97
Raman Kakotkin <t3rixis@gmail.com>
2026-02-23 18:48:04 +0300

    feat: add promo-section

---
+++ frontend/phone-company/src/app/features/home/components/promo-section/promo-section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { SvgIconComponent } from "angular-svg-icon";
+
+@Component({
+  selector: 'app-promo-section',
+  imports: [SvgIconComponent],
+  templateUrl: './promo-section.component.html',
+  styleUrl: './promo-section.component.scss',
+})
+export class PromoSectionComponent {}
